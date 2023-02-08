607026f5ec7299ac3116231b5cfb88ac0e00dbb2
2B键盘 <2968640488@qq.com>
2023-02-09 00:14:23 +0800

    feature(msrv): 引入rocketmq服务

---
+++ msrv/src/main/java/com/hantiansoft/msrv/MsrvBootstrap.java
@@ -0,0 +1,42 @@
+package com.hantiansoft.msrv;
+
+/* ************************************************************************
+ *
+ * Copyright (C) 2020 Vincent Luo All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ************************************************************************/
+
+/* Creates on 2023/2/8. */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 消息服务器
+ *
+ * @author Vincent Luo
+ */
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MsrvBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsrvBootstrap.class, args);
+    }
+
+}
